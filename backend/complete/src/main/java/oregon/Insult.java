ffce799b024fe710b19b484757d5a01c1f145e1d
VanillaEGirl <thomas.tieber@gmail.com>
2020-11-12 23:02:03 +0100

    dynamic insults backend

---
+++ backend/complete/src/main/java/oregon/Insult.java
@@ -0,0 +1,17 @@
+package oregon;
+
+public class Insult {
+    private String content;
+
+    public Insult(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
--- backend/complete/src/main/java/oregon/PromptController.java
+++ backend/complete/src/main/java/oregon/PromptController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class PromptController {
     private String promtQuestion = "Cookie essen?";
@@ -12,10 +15,24 @@ public class PromptController {
     private String choiceText3 = "Freie Liebe!";
     private String choiceText4 = "Du Schneckli";
 
+    private int insultCount = 0;
+    private List<Insult> insultList;
+
+    public PromptController() {
+        initInsultList();
+    }
+
+    private void initInsultList() {
+        insultList = new ArrayList<>();
+        insultList.add(new Insult("Hodenkobold"));
+        insultList.add(new Insult("Neger"));
+        insultList.add(new Insult("Robert"));
+        insultList.add(new Insult("Hendl"));
+    }
 
     @CrossOrigin(origins = "*")
     @GetMapping("/prompt")
-    public Prompt getPrompt(){
+    public Prompt getPrompt() {
         Prompt prompt = new Prompt();
         prompt.setQuestion(promtQuestion);
         prompt.addChoice(createTestChoice(1, choiceText1));
@@ -26,7 +43,14 @@ public class PromptController {
         return prompt;
     }
 
-    public Choice createTestChoice(int id, String choiceText){
+    @CrossOrigin(origins = "*")
+    @GetMapping("/insult")
+    public Insult getInsult() {
+        insultCount++;
+        return insultList.get(insultCount % insultList.size());
+    }
+
+    private Choice createTestChoice(int id, String choiceText) {
         Choice c = new Choice();
         c.setId(id);
         c.setChoice(choiceText);
